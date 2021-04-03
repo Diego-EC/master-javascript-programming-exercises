8f378d0d61c507214317e08dfa889fd90cfc48b7
Diego-EC <diegoezquerro@gmail.com>
2021-04-03 20:23:36 +0000

    saving progress, ej 13

--- exercises/07-removeProperty/app.js
+++ exercises/07-removeProperty/app.js
@@ -1,3 +1,15 @@
 function removeProperty(obj, key) {
   // your code here
+    for (property in obj) {
+        if (property == key) {
+            delete obj[key];
+        }
+    }
 }
+
+var obj = {
+  name: 'Sam',
+  age: 20
+}
+removeProperty(obj, 'name');
+console.log(obj.name); // --> undefined
--- exercises/11-getLengthOfTwoWords/app.js
+++ exercises/11-getLengthOfTwoWords/app.js
@@ -1,3 +1,8 @@
 function getLengthOfTwoWords(word1, word2) {
   // your code here
+  let totalWordsLenght = (word1.length) + (word2.length)
+  return(totalWordsLenght);
 }
+
+var output = getLengthOfTwoWords('some', 'words');
+console.log(output); // --> 9
